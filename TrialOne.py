fb1c0889818107b335acb2ccdd20a5149493d37c
AmeliaHe98 <AmeliaHe98@users.noreply.github.com>
2018-05-10 17:17:48 +0800

    parsing all works!!

--- TrialOne.py
+++ TrialOne.py
@@ -1,84 +1,113 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python3
 import vcf
+import glob
+import os
+# import pandas as pd
+import numpy as np
 from BCBio import GFF
 import gffutils
+import pprint
 from Bio import SeqIO
 from collections import namedtuple
 import urllib.request, urllib.parse, urllib.error
 
-#Set the number of nucleotides on the boundaries of introns that should be genotyped
-# (i.e. that should be checked for mutations). If nucleotides around the boundaries
-# of introns are mutated, this will affect splice sites and therefore very strongly
-# affect the protein structure of the gene in question. Such mutations would be equivalent
-# to nonsense mutations in terms of their effect. The default is 2.
-InNucs = 2
+InNucs = 2 #number of nucleotides on the boundaries of introns
+RecNucs = 2 #number of nucleotides on the boundaries of introns that will be written to the output text file.
+# Note that this must be a number greater than or equal to InNucs. Default 5.
 
-#Set the number of nucleotides on the boundaries of introns that will be written to the
-# output text file. Note that this must be a number greater than or equal to InNucs. Default 5.
-RecNucs = 2
+#Initialized GeneInfo named tuple. Note: namedtuple is immutable
+gffInfoFields = ["Chromosome", "Source", "SeqType", "LeftPos", "RightPos", "Score", "Orientation", "Frame", "Name"]
+GFFRecord = namedtuple("GFFRecord", gffInfoFields)
 
-##Declare directories that need to be manually set - These are directories that contain either files or programs required for the code
+VCFDir = '/Users/Amelia/Desktop/lab' #directory containing the VCFs
+VarEffDir = '/Users/Amelia/Desktop/lab' #directory of output file containing information on each variant
+RefGenomeFile = '/Users/Amelia/Desktop/lab/Cr53C.fa' #Reference genome from which variant calling was done.
+RefGenesFile = '/Users/Amelia/Desktop/lab/Creinhardtii_281_v5.5.gene_exons.gff3' #gff3 file containing the gene location and structure information requried for characterizing variants.
 
-#Set the directory containing the VCFs for which the genomic effect of their variants will be determined.
-VCFDir = '/Users/Amelia/Desktop/lab'
-
-#Set the directory into which the text file containing information on each variant will be written.
-VarEffDir = '/Users/Amelia/Desktop/lab'
-##Declare reference files that need to be manually set
-
-#Reference genome from which variant calling was done.
-RefGenomeFile = '/Users/Amelia/Desktop/lab/Cr53C.fa'
-
-#gff3 file containing the gene location and structure information requried for characterizing variants.
-RefGenesFile = '/Users/Amelia/Desktop/lab/Creinhardtii_281_v5.5.gene_exons.gff3'
-
-# gffInfoFields = ["Chromosome", "Source", "SeqType", "LeftPos", "RightPos", "Score", "Orientation", "Frame", "Name"]
-# GFFRecord = namedtuple("GFFRecord", gffInfoFields)
 
 def fa_file(fa_file):
+    '''
+    read a fasta file and return  a dictionary with
+    chromosome as key and sequence in the form of string as value
+    :param fa_file: fasta file
+    :return: dictionary
+    '''
     fasta_sequences = SeqIO.parse(open(fa_file), 'fasta')
     mydict = {}
     for seq_record in fasta_sequences:
-        #chromosome: sequence(as a string)
         mydict[seq_record.id] = str(seq_record.seq).upper()
     return mydict
 
-# def parseGFFAttributes(attributeString):
-#     ret = {}
-#     for attribute in attributeString.split(";"):
-#         key, value = attribute.split("=")
-#         ret[urllib.parse.unquote(key)] = urllib.parse.unquote(value)
-#     return ret
-
 def gff_file(gff_file):
-    open(gff_file).read()
-    # I was going to write a dictionary to store gff data before I
-    # found its library. I'm keeping this code for now in case we might
-    # use this later
-    # with open(gff_file) as infile:
-    #     for line in infile:
-    #         parts = line.strip().split("\t")
-    #         assert len(parts) == len(gffInfoFields)
-    #         # Normalize data
-    #         normalizedInfo = {
-    #             "Chromosome": urllib.parse.unquote(parts[0]),
-    #             "Source": urllib.parse.unquote(parts[1]),
-    #             "SeqType": urllib.parse.unquote(parts[2]),
-    #             "LeftPos": int(parts[3]),
-    #             "RightPos": int(parts[4]),
-    #             "Score": float(parts[5]),
-    #             "Orientation": urllib.parse.unquote(parts[6]),
-    #             "Frame": urllib.parse.unquote(parts[7]),
-    #             "Name": parseGFFAttributes(parts[8])
-    #         }
-    #         #    yield normalizedInfo
-    #         yield GFFRecord(**normalizedInfo)
+    '''
+    parse a gff file and return a dictionary
+    :param gff_file: gff file
+    :return: dictionary with name as key and everything else as value
+    '''
+    ret = {}
+    with open(gff_file) as infile:
+        for line in infile:
+            parts = line.strip().split("\t")
+            if(len(parts)==9):
+            # Normalize data
+                normalizedInfo = [
+                    urllib.parse.unquote(parts[0]),
+                    urllib.parse.unquote(parts[1]),
+                    urllib.parse.unquote(parts[2]),
+                    int(parts[3]),
+                    int(parts[4]),
+                    urllib.parse.unquote(parts[5]),
+                    urllib.parse.unquote(parts[6]),
+                    urllib.parse.unquote(parts[7]),
+                    urllib.parse.unquote(parts[8])
+                ]
+                ret[parts[8]] = normalizedInfo
+    return ret
+
+
+def vcf_file(vcf_files):
+    '''
+    read a vfc file and perform decision making for each variant file
+    :param vcf_files: vfc file
+    :return: csv file
+    '''
+    for f in vcf_files:
+        with open(f, mode='r') as vcf:
+            vcf.read()
+            #do something here with each vcf file
+
+
+def vcf_files(vcf_dir):
+    '''
+    read the directory containing vfc files
+    :param vcf_dir: vfc directory
+    :return: list of vfc files
+    '''
+    files = []
+    for filename in glob.glob(os.path.join(vcf_dir, '*.vcf')):
+        files.append(filename)
+    return vcf_file(files)
+
+
+# def cvs_files(foo):
+#     '''
+#     create a cvs file and store the variant information. Can be mutated
+#     :param not sure if needed
+#     :return: cvs file
+#     '''
+#     d = {'Chromosome', 'Position','Ref','Alt','GenomeLocation','GeneName',
+#          'mRNAName','GeneLocation','MutationType','RefCodonOrSpliceJun',
+#          'AltCodonOrSpliceJun','RefAA','AltAA'}
+#     df = pd.DataFrame(data=d)
+#     df
 
 
 def main():
     fa_file(RefGenomeFile)
     gff_file(RefGenesFile)
+    vcf_files(VCFDir)
+
 
 
 if __name__ == '__main__':
