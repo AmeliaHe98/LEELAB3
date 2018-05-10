367542735cf32ad461a48d8a09149d6323f80de0
AmeliaHe98 <AmeliaHe98@users.noreply.github.com>
2018-05-10 18:03:10 +0800

    added output file type

--- TrialOne.py
+++ TrialOne.py
@@ -3,9 +3,8 @@
 import vcf
 import glob
 import os
-# import pandas as pd
+import pandas as pd
 import numpy as np
-from BCBio import GFF
 import gffutils
 import pprint
 from Bio import SeqIO
@@ -70,8 +69,9 @@ def vcf_file(vcf_files):
     '''
     read a vfc file and perform decision making for each variant file
     :param vcf_files: vfc file
-    :return: csv file
+    :return: cvs
     '''
+    variantOutputFile = cvs_files()
     for f in vcf_files:
         with open(f, mode='r') as vcf:
             vcf.read()
@@ -85,22 +85,22 @@ def vcf_files(vcf_dir):
     :return: list of vfc files
     '''
     files = []
-    for filename in glob.glob(os.path.join(vcf_dir, '*.vcf')):
-        files.append(filename)
+    for file in glob.glob(os.path.join(vcf_dir, '*.vcf')):
+        files.append(file)
     return vcf_file(files)
 
 
-# def cvs_files(foo):
-#     '''
-#     create a cvs file and store the variant information. Can be mutated
-#     :param not sure if needed
-#     :return: cvs file
-#     '''
-#     d = {'Chromosome', 'Position','Ref','Alt','GenomeLocation','GeneName',
-#          'mRNAName','GeneLocation','MutationType','RefCodonOrSpliceJun',
-#          'AltCodonOrSpliceJun','RefAA','AltAA'}
-#     df = pd.DataFrame(data=d)
-#     df
+def cvs_files():
+    '''
+    create a cvs file and store the variant information. Can be mutated
+    :param not sure if needed
+    :return: DataFrame
+    '''
+    d = {'Chromosome':[], 'Position':[],'Ref':[],'Alt':[],'GenomeLocation':[],'GeneName':[],
+         'mRNAName':[],'GeneLocation':[],'MutationType':[],'RefCodonOrSpliceJun':[],
+         'AltCodonOrSpliceJun':[],'RefAA':[],'AltAA':[]}
+    df = pd.DataFrame(data=d)
+    return df
 
 
 def main():
